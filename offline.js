7e9b5a6dd8769517829f4b788f60a35640d11758
Unknown <punto.damar2@gmail.com>
2018-12-31 00:01:46 +0700

    export game

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1546189154,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/card-sheet0.png",
+		"images/card-sheet1.png",
+		"images/base-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
